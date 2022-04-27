42311fd9b1268f3ab1b863258bbf484e1a5cae94
Alexandr <voskhodov@gmail.com>
2022-04-27 23:02:02 +0500

    задача по возведению в степень

---
+++ 27_04_22/Zadacha_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3 в 5 степени)
+// 2, 4 -> 16
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+int result = x;
+System.Console.WriteLine("Введите степень ");
+int Stepen = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+for (int i = 2; i <= Stepen; i++)
+{
+    result *=x;
+}
+System.Console.WriteLine($"{x} в степени {Stepen} = {result}");
---
+++ 27_04_22/Zadacha_25v2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите (функцию) цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3 в 5 степени)
+// 2, 4 -> 16
+
+Console.Clear();
+
+System.Console.WriteLine("Введите число ");
+double x = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите степень ");
+double Stepen = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+double result = Math.Pow(x, Stepen);
+System.Console.WriteLine($"{x} в степени {Stepen} = {result}");
