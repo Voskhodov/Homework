fdcf5643afb1ccb33f526b2ec2d602237379c785
Alexandr <voskhodov@gmail.com>
2022-04-29 18:15:58 +0500

    решины задачи ДЗ 4 урока

---
+++ 27_04_22/Zadacha_27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите функцию, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+Console.Clear();
+void Print(int sum)
+{
+    int i = 0;
+    int y = sum;
+    while (sum !=0)
+        {
+            i = i + (sum % 10);
+            sum = sum / 10;
+
+        }
+    System.Console.WriteLine($"Сумма всех цифер в числе {y} равна {i}");
+}
+
+System.Console.Write("Введите число ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+Print(x);
---
+++ 27_04_22/Zadacha_29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из некоторого количества 
+// элементов и выводит их на экран с помощью функций. (можно спрашивать количество 
+// вводимых элементов и вводить все значения с клавиатуры)
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+Console.Clear();
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.Write("Введите размер массива ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[x];
+for (int i = 0; i < arr.Length; i++)
+{
+    System.Console.Write("Введите число для массива ");
+    int xmass = Convert.ToInt32(Console.ReadLine());
+    arr[i] = xmass;                                         //запись введенного числа в ячейку массива
+}
+Console.Clear();
+Print(arr);                                                 // вывод результата
