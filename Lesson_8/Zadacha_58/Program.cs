f724222b0a8336a63b533cfeecdf09aabf3b780b
Alexandr <voskhodov@gmail.com>
2022-05-22 10:31:01 +0500

    решена задача 60

---
+++ Lesson_8/Zadacha_58/Program.cs
@@ -0,0 +1,16 @@
+﻿int CheckSpecialNumber(int[,,] array, int minValue, int maxValue)
+{
+    int number = new Random().Next(minValue, maxValue);
+    for (int a = 0; a < array.GetLength(0); a++)
+    {
+        for (int b = 0; b < array.GetLength(1); b++)
+        {
+            for (int c = 0; c < array.GetLength(2); c++)
+            {
+                if (array[a, b, c] == number)
+                    number = CheckSpecialNumber(array, minValue, maxValue);
+            }
+        }
+    }
+    return number;
+}
---
+++ Lesson_8/Zadacha_60/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Вывод:
+// a[0, 0, 0] = 2
+// a[0, 0, 1] = 4
+
+void FillArray(int[,,] array)
+{
+    var list = new List<int>();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Boolean Proverka = true;
+                int RandomNumber = 0;
+                while (Proverka == true)
+                {
+                    RandomNumber = new Random().Next(10, 99);
+                    //System.Console.WriteLine(RandomNumber);
+                    if (list.Contains(RandomNumber) == false)
+                    {
+                        Proverka = false;
+                    }
+                }
+                //System.Console.WriteLine(list[k]);
+                array[i, j, k] = RandomNumber;
+                list.Add(RandomNumber);
+                System.Console.WriteLine($"{i},{j},{k}  =  {array[i, j, k]}");
+            }
+        }
+    }
+}
+
+int[,,] array1 = new int[3, 3, 3];
+
+FillArray(array1);
+
+System.Console.WriteLine();
