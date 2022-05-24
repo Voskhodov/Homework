9bb0f86db79f47c8d6e1da8229f6a248cf945350
Alexandr <voskhodov@gmail.com>
2022-05-24 15:20:30 +0500

    решена задача 64

--- Lesson_8/Zadacha_58/Program.cs
+++ Lesson_8/Zadacha_58/Program.cs
@@ -1,16 +1,71 @@
-﻿int CheckSpecialNumber(int[,,] array, int minValue, int maxValue)
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.Clear();
+void FillArray(int[,] array1)
 {
-    int number = new Random().Next(minValue, maxValue);
-    for (int a = 0; a < array.GetLength(0); a++)
+    for (int i = 0; i < array1.GetLength(0); i++)
     {
-        for (int b = 0; b < array.GetLength(1); b++)
+        for (int j = 0; j < array1.GetLength(1); j++)
         {
-            for (int c = 0; c < array.GetLength(2); c++)
-            {
-                if (array[a, b, c] == number)
-                    number = CheckSpecialNumber(array, minValue, maxValue);
-            }
+            array1[i, j] = new Random().Next(1, 100);
         }
     }
-    return number;
+
 }
+
+void PrintArray(int[,] array1)
+{
+    for (int i = 0; i < array1.GetLength(0); i++)
+    {
+        for (int j = 0; j < array1.GetLength(1); j++)
+        {
+            System.Console.Write($"{array1[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray2(int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[i, j] = new Random().Next(1, 100);
+        }
+    }
+}
+
+void PrintArray2(int[,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            System.Console.Write($"{array2[i, j],4}");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SummaMarixs(int[,] array3)
+{
+    int[,] con3 = new int[3, 3];
+    for (int i = 0; i < array3.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3.GetLength(1); j++)
+        {
+            array3[i, j] += array1[i, j] * array2[j, i];
+        }
+    }
+}
+
+int[,] array1 = new int[3, 3];
+int[,] array2 = new int[3, 3];
+int[,] array3 = new int[3, 3];
+
+FillArray(array1);
+PrintArray(array1);
+FillArray2(array2);
+PrintArray2(array2);
+SummaMarixs(array3);
---
+++ Lesson_9/Zadacha_64/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 64: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.Clear();
+Console.Write("Введите число m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+void PrintSumNums(int max)
+{
+    if (max < 0) max = max * -1;
+    if (max > 0)
+    {
+        x = x + max % 10;
+        max = max / 10;
+        PrintSumNums(max);
+    }
+
+
+}
+PrintSumNums(n);
+
+Console.WriteLine($"Сумма чисел ровна {x}");
