f56489f06a36ecc928740fbcaf29bfd43ddc4b63
Alexandr <voskhodov@gmail.com>
2022-05-27 17:54:01 +0500

    решена задача 58

--- Lesson_8/Zadacha_58/Program.cs
+++ Lesson_8/Zadacha_58/Program.cs
@@ -1,71 +1,58 @@
 ﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
 Console.Clear();
-void FillArray(int[,] array1)
+void FillArray(int[,] array)
 {
-    for (int i = 0; i < array1.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array1.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            array1[i, j] = new Random().Next(1, 100);
-        }
-    }
-
-}
-
-void PrintArray(int[,] array1)
-{
-    for (int i = 0; i < array1.GetLength(0); i++)
-    {
-        for (int j = 0; j < array1.GetLength(1); j++)
-        {
-            System.Console.Write($"{array1[i, j],4}");
-        }
-        System.Console.WriteLine();
-    }
-}
-
-void FillArray2(int[,] array2)
-{
-    for (int i = 0; i < array2.GetLength(0); i++)
-    {
-        for (int j = 0; j < array2.GetLength(1); j++)
-        {
-            array2[i, j] = new Random().Next(1, 100);
+            array[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
-void PrintArray2(int[,] array2)
+void PrintArray(int[,] array)
 {
-    for (int i = 0; i < array2.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < array2.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            System.Console.Write($"{array2[i, j],4}");
+            System.Console.Write($"{array[i, j],4}");
         }
         System.Console.WriteLine();
     }
 }
 
-void SummaMarixs(int[,] array3)
+int[,] MultMarixs(int[,] array1, int[,] array2)
 {
-    int[,] con3 = new int[3, 3];
+    int[,] array3;
+    if (array1.GetLength(1) != array2.GetLength(0))
+        return null;
+    array3 = new int[array1.GetLength(0), array2.GetLength(1)];
     for (int i = 0; i < array3.GetLength(0); i++)
     {
         for (int j = 0; j < array3.GetLength(1); j++)
         {
-            array3[i, j] += array1[i, j] * array2[j, i];
+            for (int k = 0; k < array1.GetLength(1); k++)
+            {
+                array3[i, j] += array1[i, k] * array2[k, j];
+            }
         }
     }
+    return array3;
 }
 
-int[,] array1 = new int[3, 3];
-int[,] array2 = new int[3, 3];
-int[,] array3 = new int[3, 3];
+int[,] array1 = new int[10, 2];
+int[,] array2 = new int[2, 10];
+int[,] array3;
 
 FillArray(array1);
 PrintArray(array1);
-FillArray2(array2);
-PrintArray2(array2);
-SummaMarixs(array3);
+System.Console.WriteLine();
+FillArray(array2);
+PrintArray(array2);
+System.Console.WriteLine();
+array3 = MultMarixs(array1, array2);
+PrintArray(array3);
+
